247e4860bc63341282799790424f1e4561362b0b
kammalage <shanantonfernando@gmail.com>
2016-02-23 18:26:56 -0800

    added superqa account test and installed custom nightwatch commands

--- tests/superqa_registration.js
+++ tests/superqa_registration.js
@@ -1,5 +1,5 @@
 module.exports = {
-/*
+
 	"Superqa Registration Test" : function (browser) {
 
  	browser
@@ -7,10 +7,41 @@ module.exports = {
  		.waitForElementVisible("body", 1000)
  		.click(".register")
  		.pause(1000)
- 		//.setValue("input[type=email]", "shanantonfernando@gmail.com")
- 		//.setValue("input[name=fullname]", "Shan Anton Fernando")
- 		.setValue(".form:nth-child(1)", "password")
- 		// .setValue("input[name=confirmPassword]","password")
+ 		.setValue("input[type=email]", "fake_email8@gmail.com")
+ 		.setValue("input[name=fullname]", "Shan Anton Fernando")
+ 		.setValue(".registrationForm input:nth-child(3)", "password")
+ 		.setValue("input[name=confirmPassword]","password")
+ 		.click("input[value=Register]")
+ 		.pause(1000)
+ 		.waitForElementVisible(".main-notice", 1000, false, function(visible) {
+
+ 				if(visible.value){
+ 					browser
+ 						.click(".login")
+ 						.pause(1000)
+ 						.setValue("input[name=email]","fake_email8@gmail.com")
+ 						.setValue("input[name=password]","password")
+ 						.click("input[type=submit")
+ 						.assert.containsText(".menuContainer","Sign out");
+
+ 				}
+ 				if(!visible.value){
+
+ 					browser
+ 						.assert.containsText(".menuContainer","Sign out");
+
+ 				}
+
+
+ 		});
  	}
- */
+
 };
+
+/*
+0. verify if user exists, if so skip to step 2
+1. register
+2. log in
+
+figure out how to test registration multiple times
+*/
